42a0535f4699744dcc7bf51bc1f13a38247b28f8
oengajohn <johnongwae4@gmail.com>
2023-10-16 07:18:12 +0300

    first commit

---
+++ src/main/java/com/systechafrica/PointOfSaleApplication.java
@@ -0,0 +1,11 @@
+package com.systechafrica;
+
+import java.io.IOException;
+
+public class PointOfSaleApplication {
+    //Logger
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
