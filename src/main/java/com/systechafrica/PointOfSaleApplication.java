35023fa79e148ecf7eccc0129d8ae66a863c77e5
oengajohn <johnongwae4@gmail.com>
2023-10-16 07:25:36 +0300

    Add logging capabalities

--- src/main/java/com/systechafrica/PointOfSaleApplication.java
+++ src/main/java/com/systechafrica/PointOfSaleApplication.java
@@ -1,11 +1,31 @@
 package com.systechafrica;
 
+import com.systechafrica.util.CustomFormatter;
 import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
 
 public class PointOfSaleApplication {
     //Logger
+    private static final Logger LOGGER = Logger.getLogger(PointOfSaleApplication.class.getName());
 
-    public static void main(String[] args) throws IOException {
+    public static void initializeLogger(){
+        FileHandler fileHandler;
+        try {
+            fileHandler = new FileHandler("log.txt");
+            CustomFormatter formatter = new CustomFormatter();
+            LOGGER.addHandler(fileHandler);
+            fileHandler.setFormatter(formatter);
+            LOGGER.info("Initialized application");
+        } catch (IOException e) {
+            LOGGER.severe(e.getMessage());
+        }
 
     }
+
+    public static void main(String[] args) throws IOException {
+        initializeLogger();
+
+    }
+
 }
